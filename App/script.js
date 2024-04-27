1cbb4554c6790de6f3f4165c5924df168f7f78e8
Winstone-Were <stoniedev@gmail.com>
2024-04-28 00:01:21 +0300

    testing image recreation

--- App/script.js
+++ App/script.js
@@ -6,8 +6,17 @@ RELOAD_BUTTON.addEventListener('click', () => {
     fetch('http://localhost:8080/myRecentTracks').then(data=> data.json().then(result=> result.forEach(res=> MusicItemElement(res.track)))).catch(err=> console.log(err));
 });
 
+
+fetch('https://p.scdn.co/mp3-preview/6a06f19d61464c9450cb44825c2ab63097f9e691?cid=0c50b00e4e6f43119040df0817ee3a04')
+    .then(data=> console.log(data))
+    .catch(err=> console.error(err));
+
+    
+
 function MusicItemElement(track) {
     
+    console.log(track);
+
     let TrackElement = document.createElement('div');
     TrackElement.className = 'track-element';
 
